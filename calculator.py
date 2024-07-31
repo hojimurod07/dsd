9a7aaf614be29661f9d127583d95f818392b9a99
hojimurod07 <hojimurad2007@gmail.com>
2024-07-31 20:39:41 +0500

    add new icon

--- calculator.py
+++ calculator.py
@@ -16,13 +16,18 @@ class Calc(QMainWindow):
 
     def navHeader(self):
         self.nav = QHBoxLayout()
+        self.nav_btn = QPushButton(" ")
+        self.nav_btn.setFixedSize(70, 50)
+        self.nav_btn.setIconSize(QSize(24, 24))
+        self.nav_btn.setIcon(QIcon('nav.png'))
+        self.nav.addWidget(self.nav_btn)
         self.nav_text = QLabel("Standart")
         font = QFont()
-        font.setPointSize(18)
+        font.setPointSize(16)
         self.nav_text.setFont(font)
         self.nav.addWidget(self.nav_text)
         self.history_btn = QPushButton("")
-        self.history_btn.setFixedSize(100, 50)
+        self.history_btn.setFixedSize(70, 50)
         self.history_btn.setIconSize(QSize(24, 24))
 
         self.history_btn.setStyleSheet("QPushButton { text-align: left; padding-left: 20px; }")
